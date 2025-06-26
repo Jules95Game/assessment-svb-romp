54db2deef6e1e806b5106350a9530a2d4d7864eb
Jules95Game <86878207+Jules95Game@users.noreply.github.com>
2025-06-26 16:08:59 +0200

    Initial Spring files

---
+++ src/main/java/com/rompware/assessmentsvb/AssessmentSvbApplication.java
@@ -0,0 +1,12 @@
+package com.rompware.assessmentsvb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentSvbApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AssessmentSvbApplication.class, args);
+  }
+}
---
+++ src/test/java/com/rompware/assessmentsvb/AssessmentSvbApplicationTests.java
@@ -0,0 +1,11 @@
+package com.rompware.assessmentsvb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentSvbApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
