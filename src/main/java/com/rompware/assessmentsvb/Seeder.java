bb4da21f7544d65d4226a38a435bf94f7766d3fb
Jules95Game <86878207+Jules95Game@users.noreply.github.com>
2025-06-27 17:18:52 +0200

    Add Seeder.java

---
+++ src/main/java/com/rompware/assessmentsvb/Seeder.java
@@ -0,0 +1,12 @@
+package com.rompware.assessmentsvb;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class Seeder implements CommandLineRunner {
+  @Override
+  public void run(String... args) throws Exception {}
+}
