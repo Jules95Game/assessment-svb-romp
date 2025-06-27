e3d6bbbf26580bcdb7831043fe096122601cd344
Jules95Game <86878207+Jules95Game@users.noreply.github.com>
2025-06-27 17:09:29 +0200

    Add getAllCustomers endpoint

--- src/main/java/com/rompware/assessmentsvb/entities/customer/CustomerController.java
+++ src/main/java/com/rompware/assessmentsvb/entities/customer/CustomerController.java
@@ -1,10 +1,19 @@
 package com.rompware.assessmentsvb.entities.customer;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/customers")
-public class CustomerController {}
+public class CustomerController {
+  private final CustomerRepository customerRepository;
+
+  @GetMapping("/all")
+  public List<Customer> getAllCustomers() {
+    return customerRepository.findAll();
+  }
+}
