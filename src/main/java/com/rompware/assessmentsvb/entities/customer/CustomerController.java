d55ffe92ae88dc0269eada1dd6a211ce26e453fe
Jules95Game <86878207+Jules95Game@users.noreply.github.com>
2025-06-27 17:03:22 +0200

    Add CustomerController.java

---
+++ src/main/java/com/rompware/assessmentsvb/entities/customer/CustomerController.java
@@ -0,0 +1,10 @@
+package com.rompware.assessmentsvb.entities.customer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/customers")
+public class CustomerController {}
