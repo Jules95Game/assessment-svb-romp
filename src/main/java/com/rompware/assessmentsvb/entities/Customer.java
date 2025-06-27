081d1902ac78223a40db97a19b6fed30101b8ff1
Jules95Game <86878207+Jules95Game@users.noreply.github.com>
2025-06-27 13:02:54 +0200

    Add Lombok annotations

--- src/main/java/com/rompware/assessmentsvb/entities/Customer.java
+++ src/main/java/com/rompware/assessmentsvb/entities/Customer.java
@@ -4,10 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import java.util.UUID;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@RequiredArgsConstructor
 public class Customer {
   @Id @GeneratedValue private UUID id;
 
-  private String name;
+  @Setter private String name;
 }
