9aea160a257ab255012cc68f2bc66e621159c656
Jules95Game <86878207+Jules95Game@users.noreply.github.com>
2025-07-01 15:49:55 +0200

    Add seedCars method

--- src/main/java/com/rompware/assessmentsvb/Seeder.java
+++ src/main/java/com/rompware/assessmentsvb/Seeder.java
@@ -1,5 +1,7 @@
 package com.rompware.assessmentsvb;
 
+import com.rompware.assessmentsvb.entities.car.Car;
+import com.rompware.assessmentsvb.entities.car.CarRepository;
 import com.rompware.assessmentsvb.entities.customer.Customer;
 import com.rompware.assessmentsvb.entities.customer.CustomerRepository;
 import java.util.List;
@@ -12,15 +14,25 @@ import org.springframework.stereotype.Component;
 public class Seeder implements CommandLineRunner {
 
   private final CustomerRepository customerRepository;
+  private final CarRepository carRepository;
 
   @Override
   public void run(String... args) throws Exception {
     seedCustomers();
+    seedCars();
   }
 
   private void seedCustomers() {
     if (customerRepository.count() > 0) return;
     List<Customer> customersToSeed = List.of(new Customer("Steve"), new Customer("Linda"));
     customerRepository.saveAll(customersToSeed);
+  }
+
+  private void seedCars() {
+    if (carRepository.count() > 0) return;
+    List<Car> carsToSeed =
+        List.of(
+            new Car("Lexus", "IS220d", "Sport", 4, 5.4, 44285, 28488.66),
+            new Car("Renault", "Master", "L2H1 T35 dCi 135 EU6 FWD", 2, 6.4, 48739.98, 30540));
   }
 }
