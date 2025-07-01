9e49e0f58a217b5be59426ab61e1a168282ad72c
Jules95Game <86878207+Jules95Game@users.noreply.github.com>
2025-07-01 16:42:13 +0200

    Add createCar endpoint

--- src/main/java/com/rompware/assessmentsvb/entities/car/CarController.java
+++ src/main/java/com/rompware/assessmentsvb/entities/car/CarController.java
@@ -2,9 +2,8 @@ package com.rompware.assessmentsvb.entities.car;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,5 +15,11 @@ public class CarController {
   @GetMapping("/all")
   public List<Car> getAllCars() {
     return carRepository.findAll();
+  }
+
+  @PatchMapping("/add")
+  public ResponseEntity<Car> createCar(@RequestBody Car newCar) {
+    Car savedCar = carRepository.save(newCar);
+    return ResponseEntity.ok(savedCar);
   }
 }
