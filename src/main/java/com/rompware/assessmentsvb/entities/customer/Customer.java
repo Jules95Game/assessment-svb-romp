bcb4759c0753f121661a94de79f8ecad4dea4286
Jules95Game <86878207+Jules95Game@users.noreply.github.com>
2025-07-01 15:59:08 +0200

    Revert Customer lombok constructor

--- src/main/java/com/rompware/assessmentsvb/entities/customer/Customer.java
+++ src/main/java/com/rompware/assessmentsvb/entities/customer/Customer.java
@@ -5,16 +5,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import java.util.UUID;
 import lombok.Getter;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
-@RequiredArgsConstructor
 public class Customer {
 
   @Id @GeneratedValue private UUID id;
 
-  @Setter @NonNull private String name;
+  @Setter private String name;
+
+  public Customer(String name) {
+    this.name = name;
+  }
 }
